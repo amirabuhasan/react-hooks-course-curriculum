fd79e7fae231ddc0b947f1e5cfa852f043db88c5
amir <amirabuhasan@gmail.com>
2020-11-25 20:55:07 +0800

    refactor posts component to hooks

--- app/components/Posts.js
+++ app/components/Posts.js
@@ -1,54 +1,59 @@
-import React from 'react'
+import React, { useReducer, useRef, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { fetchMainPosts } from '../utils/api'
 import Loading from './Loading'
 import PostsList from './PostsList'
 
-export default class Posts extends React.Component {
-  state = {
-    posts: null,
-    error: null,
-    loading: true,
-  }
-  componentDidMount() {
-    this.handleFetch()
-  }
-  componentDidUpdate(prevProps) {
-    if (prevProps.type !== this.props.type) {
-      this.handleFetch()
-    }
-  }
-  handleFetch () {
-    this.setState({
-      posts: null,
-      error: null,
-      loading: true
-    })
-
-    fetchMainPosts(this.props.type)
-      .then((posts) => this.setState({
-        posts,
-        loading: false,
-        error: null
-      }))
-      .catch(({ message }) => this.setState({
-        error: message,
+const postsReducer = (state, action) => {
+  switch (action.type) {
+    case 'success':
+      return {
+        ...state,
+        posts: action.posts,
+        error: null,
         loading: false
-      }))
+      }
+    case 'error':
+      return {
+        ...state,
+        error: action.error,
+        posts: null,
+        loading: false
+      }
   }
-  render() {
-    const { posts, error, loading } = this.state
+}
 
-    if (loading === true) {
-      return <Loading />
+const initialState = {
+  posts: null,
+  error: null,
+  loading: true
+}
+
+export default function Posts({ type }) {
+  const [state, dispatch] = useReducer(postsReducer, initialState);
+  const { error, posts, loading } = state;
+
+  useEffect(() => {
+    const handleFetch = async () => {
+      try {
+        const posts = await fetchMainPosts(type);
+        dispatch({ type: 'success', posts })
+      } catch ({ message }) {
+        dispatch({ type: 'error', error: message })
+      }
     }
+    handleFetch()
+  }, [type])
 
-    if (error) {
-      return <p className='center-text error'>{error}</p>
-    }
-
-    return <PostsList posts={posts} />
+  if (loading === true) {
+    return <Loading />
   }
+
+  if (error) {
+    return <p className='center-text error'>{error}</p>
+  }
+
+  return <PostsList posts={posts} />
 }
 
 Posts.propTypes = {
